922c848f540d95a792c8a37fcd0b3df9a1579a28
RamanKawani <RamanKhalid888@gmail.com>
2024-12-13 18:59:34 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,11 +5,8 @@ db = SQLAlchemy()
 
 class Research(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150), nullable=False)
-    author = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    filepath = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return f'<Research {self.title}>'
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500))
+    
+    # Add other fields as necessary
 
